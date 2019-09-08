15c5f610d3ba187e14d6fd1631fcfa26d0000859
Akshay Nair <phenax5@gmail.com>
2019-09-08 15:04:16 +0530

    Adds cli with retry mechanism

---
+++ cli.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const slugify = require('slugify');
+const yargs = require('yargs');
+
+const screenshit = require('./index');
+
+// randomHash :: () -> string
+const randomHash = () => `${Math.random().toString(16)}0000000`.slice(2, 8);
+
+const { argv } = yargs
+  .usage('$0 --json path/to/file.json')
+  .help()
+  .option('url', {
+    type: 'string',
+    describe: 'The url of the page you want to screenshot',
+  })
+  .option('json', {
+    type: 'string',
+    describe: 'Path to your json config file',
+  })
+  .option('csv', {
+    type: 'string',
+    describe: 'Path to your csv config file',
+  })
+  .option('outdir', {
+    type: 'string',
+    alias: 'O',
+    default: path.resolve(),
+    describe: 'Path to where you want your pngs dumped',
+  })
+  .option('dimensions', {
+    type: 'string',
+    alias: 'D',
+    default: '1024x600',
+    describe: 'The dimensions of the viewport (widthxheight)',
+  })
+  .option('retries', {
+    type: 'number',
+    alias: 'r',
+    default: 3,
+    describe: 'Max number of retries if an attempt fails',
+  });
+
+const guard = fn => options => {
+  if (!options.json && !options.csv && !options.url)
+    throw new Error('You need to specify either json or csv config file path');
+
+  return fn(options);
+};
+
+// getUrls :: { json: string, csv: string, url: string } -> []
+function getUrls({ json, csv, url }) {
+  if (url) return [url];
+  if (json) return JSON.parse(fs.readFileSync(json, 'utf-8')).urls;
+  if (csv) return JSON.parse(fs.readFileSync(csv, 'utf-8'));
+  return [];
+}
+
+// toImageFileName :: string -> string
+const toImageFileName = url =>
+  `${slugify(url).replace(/^https?:?/, '')}-${randomHash()}.png`;
+
+// takeScreenshots :: Options -> Promise [string]
+function takeScreenshots(options) {
+  const { json, csv, url, outdir, ...rest } = options;
+
+  const files = getUrls(options);
+
+  return Promise.all(
+    files.map(url => {
+      console.log('Fetching', url, '...');
+
+      return screenshit({
+        url,
+        outputPath: path.resolve(outdir, toImageFileName(url)),
+        ...rest,
+      });
+    })
+  );
+}
+
+guard(takeScreenshots)(argv)
+  .then(screenshots => {
+    if (screenshots.length === 0) {
+      console.log('No urls provided to screenshot');
+    } else {
+      screenshots.forEach(x => console.log('Saving screenshot to', x, '...'));
+    }
+
+    console.log('Done');
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+const path = require('path');
 const puppeteer = require('puppeteer');
-// const _ = require('pipey/proxy');
+const { compose } = require('pipey');
 
 const guard = fn => options => {
-  const { url, outputPath } = options || {};
+  const { url, retries, outputPath } = options || {};
 
+  if (retries < 0) throw new Error('retries cannot be less than 0');
   if (!url) throw new Error('url parameter is compulsory');
   if (!outputPath) throw new Error('outputPath parameter is compulsory');
 
   return fn(options);
 };
 
+const handleRetries = (fn, lastError) => options => {
+  const { retries } = options || {};
+  if (retries < 0) return Promise.reject(lastError);
+
+  return fn(options).catch(error => {
+    return handleRetries(fn, error)({ ...options, retries: retries - 1 });
+  });
+};
+
 async function takeScreenshot(options) {
-  const { url, outputPath } = options;
+  const { url, outputPath, timeout = 3000000, dimensions = '1024x600', logging } = options || {};
 
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -19,11 +30,23 @@ async function takeScreenshot(options) {
 
   const page = await browser.newPage();
 
-  await page.goto(url);
+  logging && console.log('Fetching', options.url, '...');
+
+  await page.goto(url, {
+    waitUntil: 'networkidle2',
+    timeout,
+  });
+
+  if (dimensions) {
+    const [width, height] = dimensions.split('x').map(x => parseInt(x, 10));
+    page.setViewport({ width, height });
+  }
 
   await page.screenshot({ path: outputPath });
 
   await browser.close();
+
+  return path.resolve(outputPath);
 };
 
-module.exports = guard(takeScreenshot);
+module.exports = compose(handleRetries, guard)(takeScreenshot);
